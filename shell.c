be46a8e74f8598e97f279fdc93a41346db603d81
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-03-10 00:31:18 +0200

    add comments

--- shell.c
+++ shell.c
@@ -9,11 +9,19 @@ typedef enum
     shell_builtin,
     executable_or_error,
 } input_type;
+
+/*
+ * Release all resources from any zombie process.
+ */
 void reap_child_zombie()
 {
     while (waitpid(-1, NULL, WNOHANG) > 0)
         ;
 }
+
+/*
+ * Track child processes terminations and record them in the file.
+ */
 void write_to_log_file()
 {
     FILE *file;
@@ -21,17 +29,30 @@ void write_to_log_file()
     fprintf(file, "Child terminated\n");
     fclose(file);
 }
+
+/*
+ * Handle termination of any child process to avoid having zombies by using signals.
+ */
 void on_child_exit()
 {
     reap_child_zombie();
     write_to_log_file();
 }
 
+/*
+ *Setup the directory of the shell to the home directory.
+ */
 void setup_environment()
 {
     chdir(getenv("HOME"));
 }
 
+/*
+ * Execute builtin commands : "cd", "echo" and "export".
+ *
+ * Parameters:
+ * - args: Array of strings representing the command and its arguments.
+ */
 void execute_shell_bultin(char *args[])
 {
     static char curdir[200];
@@ -94,6 +115,14 @@ void execute_shell_bultin(char *args[])
     }
 }
 
+/*
+ * Execute other commands by fork a subprocess and execute it using "execvp" system call.
+ * if the command has "&" argument the parent process doesn't wait for its child to terminate.
+ * if "&" argument doesn't exist the parent process waits for the termination of its child
+ *
+ * Parameters:
+ * - args: Array of strings representing the command and its arguments.
+ */
 void execute_command(char *args[])
 {
     int found_flag = 0;
@@ -126,17 +155,25 @@ void execute_command(char *args[])
     }
 }
 
-// take input from user
+/*
+ * Read the input line from user and remove the new line character .
+ */
 char *read_input()
 {
     static char line[200] = {};
     fgets(line, sizeof(line), stdin);
-    // remove the new line
     int indx = strcspn(line, "\n");
     line[indx] = '\0';
     return line;
 }
-// take the string and divide it into 2d array
+
+/*
+ * Split the input line into array of strings.
+ *
+ * Parameters:
+ * - line: Input string from user after remove newline character.
+ * - args: Array of strings representing the command and its arguments.
+ */
 void parse_input(char *line, char *args[])
 {
     args[0] = strtok(line, " ");
@@ -161,7 +198,13 @@ void parse_input(char *line, char *args[])
         }
     }
 }
-// if there is no export replace the variable with its value  ($x) replace it with the value 5
+
+/*
+ * Get the value of any variable in the environment and update the expression/command line.
+ *
+ * Parameters:
+ * - line:Input string from user after remove newline character.
+ */
 char *evaluate_expression(char *line)
 {
     char right_half[200];
@@ -185,24 +228,34 @@ char *evaluate_expression(char *line)
             strcpy(left_half, "\0");
         else
             strcpy(left_half, strchr(left_half, ' '));
-        
+
         strcpy(mid_half, getenv(mid_half));
         strcat(right_half, mid_half);
         strcat(right_half, left_half);
         strcpy(line, right_half);
-        
+
         indx = strcspn(line, "$");
         sleep(2);
     }
 
     return line;
 }
+
+/*
+ * check the type of the command.
+ */
 input_type find_type(char *arg)
 {
     if (strcmp(arg, "cd") == 0 || strcmp(arg, "echo") == 0 || strcmp(arg, "export") == 0)
         return shell_builtin;
     return executable_or_error;
 }
+
+/*
+ * Provides a simple shell interface for command execution.
+ * The shell runs in a loop, allowing users to input commands and executes them.
+ * The loop continues until the user enters the "exit" command.
+ */
 void shell()
 {
     int exit_now = 0;
